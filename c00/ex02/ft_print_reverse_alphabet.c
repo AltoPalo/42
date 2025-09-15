4f92f70f6a2ad9a19b7e57c0fbe917263d2bf0b8
Ilyes Elie Amara <iamara@f1r7s21.clusters.42paris.fr>
2025-09-15 11:02:24 +0200

    test

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iamara <iamara@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 14:43:28 by iamara            #+#    #+#             */
+/*   Updated: 2025/09/14 09:38:12 by iamara           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	n;
+
+	n = 'z';
+	while (n >= 'a')
+	{
+		write(1, &n, 1);
+		n--;
+	}
+}
+/*int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return(0);
+}*/
